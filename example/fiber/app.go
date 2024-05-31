1c1aad2568179ed2f387ab0eae893e584f0eeaa3
Zohurul Islam <mail@zohurul.com>
2024-05-31 15:55:02 +0600

    update app.go in fiber example

--- example/fiber/app.go
+++ example/fiber/app.go
@@ -5,12 +5,11 @@ import (
 )
 
 func main() {
+	InitApp()
 
 	onReqDispatcher, _ := goevent.GetDispatcher[OnRequestEvent]("fiber.OnRequest")
 	onReqDispatcher.Subscribe("fiber.OnRequest", func(e OnRequestEvent) {
 		e.Ctx.SendString("Hello, World 👋!")
 	}, 1)
-
-	InitApp()
 
 }
